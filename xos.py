658000033cc7dd8286ebbfaa32d83e42ad941c31
deekshithasvishakanta0 <114645641+deekshithasvishakanta0@users.noreply.github.com>
2023-12-22 18:28:44 +0530

    Update xos.py

--- xos.py
+++ xos.py
@@ -1,16 +1,25 @@
 import requests
-from requests_html import HTML
 
-base_url = 'https://stackoverflow.com/questions/tagged/python'
-tag="questions"
-query_filter="Votes"
-url = f"{base_url}{tag}?tab={query_filter}"
-url
+from bs4 import BeautifulSoup
+import pandas as pd
 
-r = requests.get(url)
-html_str= r.text
+# URL of the website to scrape
+url = "https://stackoverflow.com/questions/tagged/python"
 
-html = HTML(html=html_str)
+# Send an HTTP GET request to the website
+response = requests.get(url)
 
-question_classes = html.find("s-link")
-print(question_classes)
+# Parse the HTML code using BeautifulSoup
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# Extract the relevant information from the HTML code
+questions = []
+for row in soup.select('div.s-post-summary--content'):
+    questions = row.find('h3', class_='.s-post-summary--content-title').find('a').get_text()
+    questions.append([questions])
+
+# Store the information in a pandas dataframe
+    df = pd.DataFrame(questions, columns=['questions'])
+
+# Add a delay between requests to avoid overwhelming the website with requests
+    df.to_csv('main.csv', index=False)
